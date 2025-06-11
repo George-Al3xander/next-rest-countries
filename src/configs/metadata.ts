4c9f5721fac7402300ad62b4a5ad5d3dfb6e7e1e
George <george.valuiskyi@gmail.com>
2025-06-11 18:24:33 +0300

    feat: metadata config
    Implement and integrate the metadata config component.

---
+++ src/configs/metadata.ts
@@ -0,0 +1,5 @@
+export const metadata = {
+    title: "Where in the world?",
+    description:
+        "Explore every country worldwide. Search, filter by region, and delve into comprehensive country data, including navigable border countries. Enjoy a fully responsive design and personalize your view with light and dark mode toggles.",
+} as const;
