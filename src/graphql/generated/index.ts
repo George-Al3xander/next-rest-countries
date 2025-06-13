1593fffd333e7d8c87b0dd9e7e2d47ce5135508a
George <george.valuiskyi@gmail.com>
2025-06-13 22:10:39 +0300

    feat: graphql codegen
    Add GraphQL codegen configuration to generate TypeScript types.
    Include initial getCountries query and GraphQL config setup.
    Generated output is written to src/graphql/generated/index.ts.

---
+++ src/graphql/generated/index.ts
@@ -0,0 +1,183 @@
+import { GraphQLClient, RequestOptions } from "graphql-request";
+import gql from "graphql-tag";
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+    [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+    [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+    [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+    T extends { [key: string]: unknown },
+    K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+    | T
+    | {
+          [P in keyof T]?: P extends " $fragmentName" | "__typename"
+              ? T[P]
+              : never;
+      };
+type GraphQLClientRequestHeaders = RequestOptions["requestHeaders"];
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+    ID: { input: string; output: string };
+    String: { input: string; output: string };
+    Boolean: { input: boolean; output: boolean };
+    Int: { input: number; output: number };
+    Float: { input: number; output: number };
+};
+
+export type Country = {
+    __typename?: "Country";
+    alpha2Code: Scalars["String"]["output"];
+    alpha3Code: Scalars["String"]["output"];
+    altSpellings?: Maybe<Array<Scalars["String"]["output"]>>;
+    area?: Maybe<Scalars["Float"]["output"]>;
+    borders?: Maybe<Array<Scalars["String"]["output"]>>;
+    callingCodes: Array<Scalars["String"]["output"]>;
+    capital?: Maybe<Scalars["String"]["output"]>;
+    cioc?: Maybe<Scalars["String"]["output"]>;
+    currencies?: Maybe<Array<Currency>>;
+    demonym: Scalars["String"]["output"];
+    flag: Scalars["String"]["output"];
+    flags: Flag;
+    gini?: Maybe<Scalars["Float"]["output"]>;
+    independent: Scalars["Boolean"]["output"];
+    languages: Array<Language>;
+    latlng?: Maybe<Array<Scalars["Float"]["output"]>>;
+    name: Scalars["String"]["output"];
+    nativeName: Scalars["String"]["output"];
+    numericCode: Scalars["String"]["output"];
+    population: Scalars["Float"]["output"];
+    region: Scalars["String"]["output"];
+    regionalBlocs?: Maybe<Array<RegionalBloc>>;
+    subregion: Scalars["String"]["output"];
+    timezones: Array<Scalars["String"]["output"]>;
+    topLevelDomain: Array<Scalars["String"]["output"]>;
+    translations: Translations;
+};
+
+export type Currency = {
+    __typename?: "Currency";
+    code: Scalars["String"]["output"];
+    name: Scalars["String"]["output"];
+    symbol: Scalars["String"]["output"];
+};
+
+export type Flag = {
+    __typename?: "Flag";
+    png: Scalars["String"]["output"];
+    svg: Scalars["String"]["output"];
+};
+
+export type Language = {
+    __typename?: "Language";
+    iso639_1?: Maybe<Scalars["String"]["output"]>;
+    iso639_2?: Maybe<Scalars["String"]["output"]>;
+    name: Scalars["String"]["output"];
+    nativeName?: Maybe<Scalars["String"]["output"]>;
+};
+
+export type Query = {
+    __typename?: "Query";
+    countries: Array<Country>;
+};
+
+export type RegionalBloc = {
+    __typename?: "RegionalBloc";
+    acronym: Scalars["String"]["output"];
+    name: Scalars["String"]["output"];
+    otherNames?: Maybe<Array<Scalars["String"]["output"]>>;
+};
+
+export type Translations = {
+    __typename?: "Translations";
+    br: Scalars["String"]["output"];
+    de: Scalars["String"]["output"];
+    es: Scalars["String"]["output"];
+    fa?: Maybe<Scalars["String"]["output"]>;
+    fr: Scalars["String"]["output"];
+    hr: Scalars["String"]["output"];
+    hu: Scalars["String"]["output"];
+    it: Scalars["String"]["output"];
+    ja: Scalars["String"]["output"];
+    nl: Scalars["String"]["output"];
+    pt: Scalars["String"]["output"];
+};
+
+export type GetCountriesQueryVariables = Exact<{ [key: string]: never }>;
+
+export type GetCountriesQuery = {
+    __typename?: "Query";
+    countries: Array<{
+        __typename?: "Country";
+        name: string;
+        flag: string;
+        population: number;
+        region: string;
+        capital?: string | null;
+        alpha2Code: string;
+    }>;
+};
+
+export const GetCountriesDocument = gql`
+    query getCountries {
+        countries {
+            name
+            flag
+            population
+            region
+            capital
+            alpha2Code
+        }
+    }
+`;
+
+export type SdkFunctionWrapper = <T>(
+    action: (requestHeaders?: Record<string, string>) => Promise<T>,
+    operationName: string,
+    operationType?: string,
+    variables?: any,
+) => Promise<T>;
+
+const defaultWrapper: SdkFunctionWrapper = (
+    action,
+    _operationName,
+    _operationType,
+    _variables,
+) => action();
+
+export function getSdk(
+    client: GraphQLClient,
+    withWrapper: SdkFunctionWrapper = defaultWrapper,
+) {
+    return {
+        getCountries(
+            variables?: GetCountriesQueryVariables,
+            requestHeaders?: GraphQLClientRequestHeaders,
+            signal?: RequestInit["signal"],
+        ): Promise<GetCountriesQuery> {
+            return withWrapper(
+                (wrappedRequestHeaders) =>
+                    client.request<GetCountriesQuery>({
+                        document: GetCountriesDocument,
+                        variables,
+                        requestHeaders: {
+                            ...requestHeaders,
+                            ...wrappedRequestHeaders,
+                        },
+                        signal,
+                    }),
+                "getCountries",
+                "query",
+                variables,
+            );
+        },
+    };
+}
+export type Sdk = ReturnType<typeof getSdk>;
