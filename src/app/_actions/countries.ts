568c56356a320edf2b4250911847a24cdec1da84
George <george.valuiskyi@gmail.com>
2025-06-16 14:11:35 +0300

    feat: get-country action
    Add getCountry method to the public country service layer, forwarding
    calls to internalApi.countries.getByCode.

--- src/app/_actions/countries.ts
+++ src/app/_actions/countries.ts
@@ -1,5 +1,9 @@
 import * as internalApi from "@/services/api/internal";
-import { CountryBasic } from "@/types/models/country";
+import { CountryBasic, CountryExtended } from "@/types/models/country";
 
 export const getCountries = async (): Promise<CountryBasic[]> =>
     await internalApi.countries.findAll();
+
+export const getCountry = async (
+    ...args: Parameters<typeof internalApi.countries.getByCode>
+): Promise<CountryExtended> => await internalApi.countries.getByCode(...args);
