5f3f41421d512a54c6122c41fc9a0d4a148a0bfa
George <george.valuiskyi@gmail.com>
2025-06-20 19:18:14 +0300

    fix: add suspense in subheader
    Wrap SearchBar and RegionsMenu components with React Suspense to fix
    build issues on Vercel related to async rendering and component loading.

--- src/app/_components/Subheader.tsx
+++ src/app/_components/Subheader.tsx
@@ -2,6 +2,7 @@
 
 import { RegionsMenu } from "@/app/_components/RegionsMenu";
 import { useParams } from "next/navigation";
+import { Suspense } from "react";
 import { BackButton } from "./BackButton";
 import { SearchBar } from "./SearchBar";
 
@@ -11,9 +12,9 @@ export const Subheader = () => {
     if ("code" in params) return <BackButton />;
 
     return (
-        <>
+        <Suspense>
             <SearchBar />
             <RegionsMenu />
-        </>
+        </Suspense>
     );
 };
