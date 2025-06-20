0b9633c0acb8e173d259c264a215822f80f2097c
George <george.valuiskyi@gmail.com>
2025-06-20 15:16:52 +0300

    refactor: graphql api url env var -> client
    Update environment variable usage from GRAPHQL_API_URL to
    NEXT_PUBLIC_GRAPHQL_API_URL for client-side compatibility.
    Modify env config and Apollo client initialization accordingly.

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,10 +2,10 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-    server: {
-        GRAPHQL_API_URL: z.string().url(),
+    client: {
+        NEXT_PUBLIC_GRAPHQL_API_URL: z.string().url(),
     },
     runtimeEnv: {
-        GRAPHQL_API_URL: process.env.GRAPHQL_API_URL,
+        NEXT_PUBLIC_GRAPHQL_API_URL: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
     },
 });
--- src/services/libs/internalClient.ts
+++ src/services/libs/internalClient.ts
@@ -2,6 +2,6 @@ import { env } from "@/configs/env";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const internalClient = new ApolloClient({
-    uri: env.GRAPHQL_API_URL,
+    uri: env.NEXT_PUBLIC_GRAPHQL_API_URL,
     cache: new InMemoryCache(),
 });
